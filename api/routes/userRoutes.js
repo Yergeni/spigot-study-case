16461c25d942c547db0c96dba8d84cf2d0a2f981
Yergeni Cabranes <yergenis@gmail.com>
2023-06-15 16:13:46 -0400

    refactor: user routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -4,14 +4,16 @@ import {
 	logoutUser,
 	registerUser,
   userProfile,
-} from "../controllers/user-controller.js";
-import { validateAuthorization } from "../utils.js";
+} from "../controllers/userController.js";
+
+import validateAuthorization from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
-router.post("/logout", validateAuthorization, logoutUser);
+router.post("/logout", logoutUser);
 router.get("/profile", validateAuthorization, userProfile);
 
 export default router;
